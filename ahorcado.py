b85c3f86484c0721daa785590555065d62c9a73c
Alexis Medina <82667507+alexisnpavlik@users.noreply.github.com>
2021-07-11 22:42:38 -0300

    problema

--- ahorcado.py
+++ ahorcado.py
@@ -1,8 +1,9 @@
 import random
 import os
+import re
 
 def lectura():
-    archivo=open("./data.txt","r",encoding="utf-8") 
+    archivo=open("c:/Users/elbus/Documents/Python/Ahorcado/data.txt","r",encoding="utf-8") 
     archivo= archivo.read()
     lista_palabras=archivo.split()
     return lista_palabras
@@ -11,11 +12,44 @@ def elegir_palabra():
     return random.choice(lectura())
     
 def interfaz(palabra_correcta,respuesta):
+    respuesta=""
     os.system ("cls")
-    print("Adivina la palabra en el juego del ahorcado")
+    print("Adivina una letra en el juego del ahorcado")
+
     for i in range(len(palabra_correcta)):
-        respuesta.append("_")
-        print(respuesta[i]," ",end="")
+        respuesta=respuesta+"_"
+    print(respuesta)
+    print(palabra_correcta)
+
+
+    while respuesta!=palabra_correcta:
+        print(respuesta)
+
+        letra= input("Ingresa una letra: ")
+
+
+        for x in enumerate(palabra_correcta):
+            if letra==x[1]:
+                os.system ("cls")
+                print("¡¡¡Corecto!!!!")
+
+                print(type(x[0]))
+                print(type(x[1]))
+                e=x[0]
+                f="ss"
+                respuesta.replace(respuesta[e],f)
+                break
+            
+
+
+            
+            else:
+                os.system ("cls")
+                print("¡¡¡Mala Suerte!!!!")
+                print(respuesta)
+
+        
+
 
 
 def comprobar_respuesta():
